8050fc8bfa4bc7fc6d211101cda478c298581e4c
Ashish Kr. Pandey <akp9117409798@gmail.com>
2025-02-01 22:20:18 +0530

    feat: add About and Contact pages with initial content

---
+++ src/app/pages/ContactPage.tsx
@@ -0,0 +1,20 @@
+const Contact = () => {
+  return (
+    <>
+      <main className="max-w-4xl mx-auto p-6 dark:text-white text-black py-20 md:py-28">
+        <h1 className="text-4xl font-bold mb-4">Contact Us</h1>
+        <p className="text-lg mb-6">
+          Welcome to{" "}
+          <span className="text-blue-400 font-semibold">TechTalks</span>! Our
+          mission is to share insightful articles, tutorials, and resources to
+          help developers stay updated with the latest trends in technology.
+        </p>
+        <p className="text-black dark:text-gray-300 mt-2">
+          We're working hard to bring you this page. Stay tuned for updates!
+        </p>
+      </main>
+    </>
+  );
+};
+
+export default Contact;
